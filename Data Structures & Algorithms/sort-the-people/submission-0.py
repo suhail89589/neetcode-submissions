c700095110b5ab12e6d5cf65834220743a1f50c3
Suhail Malik <mohdsuhail8958911@gmail.com>
2026-08-31 16:11:38 +0530

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+       paired = zip(names, heights)
+        
+        
+       sorted_pairs = sorted(paired, key=lambda x: x[1], reverse=True)
+        
+       
+       return [name for name, height in sorted_pairs]
